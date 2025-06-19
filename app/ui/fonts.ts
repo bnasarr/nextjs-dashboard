7bc71ce2fe83c93e4d95bfbf4302be663f8c61eb
Bakhtawar Nasar <bakhtawarrnasar@gmail.com>
2025-06-20 02:09:06 +0500

    add preload prop to fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,9 +2,11 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
+  preload: true,
 });
 
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
+  preload: true,
 });
