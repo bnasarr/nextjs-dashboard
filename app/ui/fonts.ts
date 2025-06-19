d5f20c913719f17ca7b0f192c8f0b98169b5cf9a
Bakhtawar Nasar <bakhtawarrnasar@gmail.com>
2025-06-20 02:20:19 +0500

    perload false

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,11 +2,10 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
-  preload: true,
 });
 
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
-  preload: true,
+  preload: false,
 });
