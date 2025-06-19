21f00d4c315c3db180b5cc8a12d6756fec72bbda
Bakhtawar Nasar <bakhtawarrnasar@gmail.com>
2025-06-20 01:11:42 +0500

    Fix font subset for Lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,8 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subset: ["latin"] });
 
-export const lusitana = Lusitana({ subset: ["latin"], weight: ["400", "700"] });
+export const lusitana = Lusitana({
+  subset: ["latin"],
+  weight: ["400", "700"],
+  preload: true,
+});
