a8c1ce8d513d6102df0b2e79ec27992803364167
Bakhtawar Nasar <bakhtawarrnasar@gmail.com>
2025-06-20 17:39:25 +0500

    fonts issue

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,11 +2,9 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
-  preload: false,
 });
 
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
-  preload: false,
 });
